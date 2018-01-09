ab053594d37adb7015efffce78f81014a3b33455
xyyx <xyyx@mail.ru>
2018-01-09 19:36:56 +0100

    Unlock keystore with fingerprint after reboot (1/2)
    Change-Id: I839e6764760cee1a309c47e73ae58a337b033d7a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4942,6 +4942,10 @@ public final class Settings {
          */
         public static final String CHOOSER_ACTIVITY_BLACKLIST = "chooser_activity_blacklist";
 
+        /** Unlock keystore with fingerprint after reboot
+         * @hide
+         */
+        public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
