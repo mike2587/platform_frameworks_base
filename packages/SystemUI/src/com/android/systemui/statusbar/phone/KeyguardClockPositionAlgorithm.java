5f312e2a53afeed680bb98178070fdcc51ef3834
mike2587 <mike.gine@gmail.com>
2018-06-04 13:51:49 +0200

    Apply mBigClockPadding to Sammy style and increase padding

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardClockPositionAlgorithm.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardClockPositionAlgorithm.java
@@ -184,7 +184,7 @@ public class KeyguardClockPositionAlgorithm {
     }
 
     public void setClockSelection(int style) {
-        mIsBigClock = style == 3;
+        mIsBigClock = style == 3 || style == 4 || style== 5;
     }
 
     private float burnInPreventionOffsetY() {
